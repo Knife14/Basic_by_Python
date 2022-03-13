ff3abc9e7d82b7a71eed5877e2403f12c895fe2f
Knife14 <67036782+Knife14@users.noreply.github.com>
2022-03-13 13:43:21 +0800

    遍历中断 / 标志数

--- EXERCISE/ListArray/findNumberIn2DArray.py
+++ EXERCISE/ListArray/findNumberIn2DArray.py
@@ -15,7 +15,27 @@ example：
 给定 target = 20，返回 false。
 """
 
+# 简单做法： 遍历n行，根据递增原则逐列判断，若出现遍历值小于但不等于目标值的情况，则跳过该行
+# 时间复杂度： O（N + MlogN） 空间复杂度： O（1）
+class Solution:
+    def findNumberIn2DArray(self, matrix: List[List[int]], target: int) -> bool:
+        if not matrix:
+            return False
 
+        n, m = len(matrix), len(matrix[0])
+
+        for i in range(n):
+            for j in range(m - 1, -1, -1):
+                if target > matrix[i][j]:
+                    break
+                elif target == matrix[i][j]:
+                    return True
+
+
+        return False
+
+# 标志数： 利用二叉树左小右大的定义，从矩阵左下角开始遍历判断，移动的方向只有上或者右
+# 时间复杂度： O（M + N）  空间复杂度： O（1）
 class Solution:
     def findNumberIn2DArray(self, matrix: list, target: int) -> bool:
 
