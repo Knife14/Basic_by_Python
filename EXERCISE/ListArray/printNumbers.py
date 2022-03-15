47b452cb3b865b5ca9855c9d5282d128ffbf366f
Knife14 <67036782+Knife14@users.noreply.github.com>
2022-03-15 17:43:03 +0800

    数学方法

--- EXERCISE/ListArray/printNumbers.py
+++ EXERCISE/ListArray/printNumbers.py
@@ -7,22 +7,31 @@ example:
 输出: [1,2,3,4,5,6,7,8,9]
 """
 
-
+# 直接遍历：遍历个数为 10 ** n - 1
+# 时间复杂度： O(2^n)
 class Solution:
     def printNumbers(self, n: int) -> list:
         nums = []
-
-        """
-        l = 1
-        for i in range(n):
-            l *= 10
-        l -= 1
-        """
 
         for i in range(1, 10 ** n):
             nums.append(i)
 
         return nums
+
+# 先统计出会打印多少数值：根据 9 + 10 ** （ n - 1 ）
+# 时间复杂度：O(n)
+class Solution:
+    def printNumbers(self, n: int) -> List[int]:
+        nums = 0
+
+        for i in range(1, n + 1):
+            if i == 1:
+                nums += 9
+                continue
+
+            nums += 9 * (10 ** (i - 1))
+
+        return list(range(1, nums + 1))
 
 
 if __name__ == '__main__':
