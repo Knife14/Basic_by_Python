405008b4ef42fd14989f77080698ae932f237fcf
Knife14 <67036782+Knife14@users.noreply.github.com>
2022-03-24 20:46:53 +0800

    深度搜索 + 中序遍历 / 暴力检索

--- EXERCISE/BST/kthLargest.py
+++ EXERCISE/BST/kthLargest.py
@@ -22,6 +22,35 @@ class TreeNode:
         self.right = None
 
 
+# DFS + 中序遍历（右中左），根据k值的递减归零提前退出
+# 时间复杂度： O（N）  空间复杂度：O（N）
+class Solution:
+    def kthLargest(self, root: TreeNode, k: int) -> int:
+        self.k = k
+        self.res = -1
+
+        def dfs(curr: TreeNode):
+            if not curr:
+                return 
+
+            dfs(curr.right)
+
+            # quit dfs(curr.left) or more dfs(curr.right) when self.k == 0
+            if self.k == 0:
+                return
+            self.k -= 1
+            # after self.k - 1 == 0, it get the res
+            if self.k == 0:
+                self.res = curr.val
+
+            dfs(curr.left)
+
+        dfs(root)
+        return self.res
+      
+      
+# 遍历整颗树然后进行排序
+# 空间复杂度：O（N）   时间复杂度：O（2N）
 class Solution:
     def kthLargest(self, root: TreeNode, k: int) -> int:
 
