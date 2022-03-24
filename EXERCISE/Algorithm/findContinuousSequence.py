a6bafc6c12aee1e3826099d1b84139213d327464
Knife14 <67036782+Knife14@users.noreply.github.com>
2022-03-24 21:36:39 +0800

    滑动窗口 + 双指针

--- EXERCISE/Algorithm/findContinuousSequence.py
+++ EXERCISE/Algorithm/findContinuousSequence.py
@@ -2,7 +2,7 @@
 title: 和为s的连续正数序列
 writer: 山客
 date: 2021.4.18
-key：滑动窗口
+key：滑动窗口，双指针
 example:
 输入：target = 9
 输出：[[2,3,4],[4,5]]
@@ -11,26 +11,21 @@ example:
 输出：[[1,2,3,4,5],[4,5,6],[7,8]]
 """
 
+# 时间复杂度：O（N）    空间复杂度：O（1）
 class Solution:
     def findContinuousSequence(self, target: int) -> list:
-        res = []
-        i, j = 1, 1  # 滑动窗口的左右边界
-        tmp = 0  # 滑动窗口的和
+        mid  = target // 2
 
-        while i <= target // 2:
-            # i最长只能到目标值的一半，由于tar// 2 * 2 + 1 <= target
+        res = []
+        i, j, tmp = 1, 1, 0
+        while i <= mid:
             if tmp < target:
                 tmp += j
                 j += 1
-            elif tmp > target:
-                tmp -= i
-                i += 1
             else:
-                # tmp == target
-                arr = list(range(i, j))
-                res.append(arr)
-                # 继续移动
+                if tmp == target:
+                    res.append(list(range(i, j)))
                 tmp -= i
                 i += 1
-
+        
         return res
