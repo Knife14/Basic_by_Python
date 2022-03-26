c7f770c597d5983e6871f633207675a09afcabda
Knife14 <67036782+Knife14@users.noreply.github.com>
2022-03-26 13:07:44 +0800

    DFS

--- EXERCISE/BST/lowestCommonAncestor.py
+++ EXERCISE/BST/lowestCommonAncestor.py
@@ -19,13 +19,25 @@ class TreeNode:
         self.left = left
         self.right = right
 
-
+# 一共有四种情况：都在左 / 右子树里，都不在，分别在左右子树
+# 空间复杂度：O（N）    时间复杂度：O（N）
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        # 若分别在左右子树，则他们的公共祖先节点为根节点
-        if root.val < p.val and root.val < q.val:
-            return self.lowestCommonAncestor(root.right, p, q)
-        if root.val > p.val and root.val > q.val:
-            return self.lowestCommonAncestor(root.left, p, q)
+        if not root or root == p or root == q:
+            return root
+        
+        left = self.lowestCommonAncestor(root.left, p, q)
+        right = self.lowestCommonAncestor(root.right, p, q)
 
-        return root
+        if not left and right:
+            # must be not in left, so return right
+            return right
+        elif not right and left :
+            # must be not in right, so return left
+            return left
+        elif left and right:
+            # if all in, it makes sure the father node is the root 
+            return root
+        else:
+            # if all not in, it makes sure there is not the father node
+            return 
