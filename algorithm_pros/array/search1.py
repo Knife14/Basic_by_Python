9a912696a23ada4ebd38870b4d6a2fee36b60782
Smoothly <value_1218@163.com>
2023-01-30 13:29:51 +0800

    搜索旋转排序数组，二分查找，注意条件

--- algorithm_pros/array/search1.py
+++ algorithm_pros/array/search1.py
@@ -2,6 +2,7 @@
 title：搜索旋转排序数组①
 writer：山客
 date：2021.4.7
+key：二分查找
 example：
 输入：nums = [4,5,6,7,0,1,2], target = 0
 输出：4
@@ -14,24 +15,36 @@ tips：
 
 
 class Solution:
-    def search(self, nums: list, target: int) -> int:
-
-        l, r = 0, len(nums) - 1
-
-        while l < r:
-            mid = (l + r) // 2
+    def search(self, nums: list[int], target: int) -> int:
+        left, right = 0, len(nums) - 1
+        while left <= right:
+            mid = (left + right) // 2
             if nums[mid] == target:
                 return mid
+            
+            # 0 -> mid 升序，不一定遇到最大值            
             if nums[0] <= nums[mid]:
-                if nums[0] <= target <= nums[mid]:
-                    # 二分法重新确定边界
-                    r = mid - 1
+                # 只有在target大于nums[0]以及小于mid索引值时
+                # target一定在mid左边
+                if nums[0] <= target < nums[mid]:
+                    right = mid - 1
+                # target 大于mid索引值时，一定会在mid右边
+                # target 小于mid索引值时，却不一定会在mid的左边，即if的条件
                 else:
-                    l = mid + 1
+                    left = mid + 1
+            # 0 -> mid 先升序后遇到最大值
             else:
-                if nums[mid] <= target <= nums[len(nums) - 1]:
-                    l = mid + 1
+                # 只有 target 大于mid索引值并且小于nums[-1]值时
+                # target一定会在mid右边
+                if nums[mid] < target <= nums[-1]:
+                    left = mid + 1
+                # target小于mid索引值时，target一定在mid左边
+                # target大于mid索引值时，target不一定在mid的左边，即if的条件
                 else:
-                    r = mid - 1
-
+                    right = mid - 1
+        
         return -1
+
+a = Solution()
+n = [4,5,6,7,0,1,2]
+print(a.search(n, 3))
